d681d13a4cf80a12cc3cb5078b1c3946913bef92
chryswds <dybas.chrys@gmail.com>
2024-11-05 13:38:26 +0000

    creating methods to perform actions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,19 +10,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-
         //Array list storing player choices
         ArrayList<Algomon> player1Choices = new ArrayList<Algomon>();
         ArrayList<Algomon> player2Choices = new ArrayList<Algomon>();
-
-
         //Scanner for user input
         Scanner myKB = new Scanner(System.in);
-
         System.out.println("Player 1 choose your 4 algomons: ");
-
-
         //for loop to get user choice
         for(int i = 0; i < 4; i++){
             System.out.println("Choose your algomons " + (i + 1) + ":");
@@ -34,10 +27,7 @@ public class Main {
             System.out.println("6.MoonCrawler - MoonType");
             System.out.println("7.StarLight - StarType");
             System.out.println("8.SunnyCorn - SunType");
-
             int choice = myKB.nextInt();
-
-
             //storing user choices in the array
             switch(choice){
                 case 1:
@@ -68,14 +58,8 @@ public class Main {
                     System.out.println("Invalid choice");
                     break;
             }
-
-
         }
-
-
         System.out.println("Player 2 choose your 4 algomons: ");
-
-
         //for loop to get user choice
         for(int i = 0; i < 4; i++){
             System.out.println("Choose your algomons " + (i + 1) + ":");
@@ -87,10 +71,7 @@ public class Main {
             System.out.println("6.MoonCrawler - MoonType");
             System.out.println("7.StarLight - StarType");
             System.out.println("8.SunnyCorn - SunType");
-
             int choice = myKB.nextInt();
-
-
             //storing choices into the arraylist
             switch(choice){
                 case 1:
@@ -121,42 +102,78 @@ public class Main {
                     System.out.println("Invalid choice");
                     break;
             }
-
-
         }
-
-        myKB.close();
-
 
         //printing player choices
         System.out.println("player 1 choices:");
-
         for(int i = 0; i < player1Choices.size(); i++) {
-            System.out.print(player1Choices.get(i).getName()+ "\n ");
+            System.out.print(player1Choices.get(i).getName()+ " - ");
         }
-
         System.out.println("\nplayer 2 choices:");
-
         for(int i = 0; i < player2Choices.size(); i++) {
-            System.out.print(player2Choices.get(i).getName() + "\n ");
+            System.out.print(player2Choices.get(i).getName() + " - ");
         }
 
 
-
-
-
-
-
-
-
-
-
+        performAction(player1Choices, 1);
+        performAction(player2Choices, 2);
 
 
     }
 
 
+    public static void performAction(ArrayList<Algomon> playerChoices, int playerNumber){
+        Scanner myKB = new Scanner(System.in);
+        System.out.println("\n Player " + playerNumber + " !!! choose the action you want to perform : ");
+        System.out.println("1. Offensive");
+        System.out.println("2. Defensive");
+        System.out.println("3. Healing");
 
+        int actionChoice = myKB.nextInt();
+        Actions.Action action = null;
+
+        // Determine action based on user's choice
+        switch (actionChoice) {
+            case 1:
+                action = Actions.Action.OFFENSIVE;
+                break;
+            case 2:
+                action = Actions.Action.DEFENSIVE;
+                break;
+            case 3:
+                action = Actions.Action.HEALING;
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+        }
+
+        // Prompt to select which Algomon to perform the action
+        System.out.println("Choose an Algomon to perform the action:");
+        for (int j = 0; j < playerChoices.size(); j++) {
+            System.out.println((j + 1) + ". " + playerChoices.get(j).getName());
+        }
+        int algomonChoice = myKB.nextInt() - 1; // Subtract 1 to use it as an index
+
+        // Validate Algomon choice
+        if (algomonChoice < 0 || algomonChoice >= playerChoices.size()) {
+            System.out.println("Invalid choice. Please try again.");
+        }
+
+        // Perform the action based on the selected Algomon
+        Algomon selectedAlgomon = playerChoices.get(algomonChoice);
+        switch (action) {
+            case OFFENSIVE:
+                System.out.println(((Actions) selectedAlgomon).attack());
+                break;
+            case DEFENSIVE:
+                System.out.println(((Actions) selectedAlgomon).defense());
+                break;
+            case HEALING:
+                System.out.println(((Actions) selectedAlgomon).heal());
+                break;
+        }
+
+    }
 
 
 }
